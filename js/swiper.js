11d189e833a32dbbd6a910c8d53519e919ba251c
Lucas <st26078@ormiston.school.nz>
2026-09-22 19:44:05 +1200

    Added clickable pagination to the swiper

--- js/swiper.js
+++ js/swiper.js
@@ -4,5 +4,9 @@ new Swiper('#swiper-1',{
     effect: 'fade',
     fadeEffect:{
         crossFade: true
+    },
+    pagination:{
+        el: "#swiper-1 .swiper-pagination",
+        clickable: true
     }
 });
